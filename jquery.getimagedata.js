74ed2cb77981a69a921caa2fc55c10af668a3bf3
Max Novakovic <max@maxnov.com>
2011-01-13 15:19:43 +0000

    Final ammend for version 0.3

--- jquery.getimagedata.js
+++ jquery.getimagedata.js
@@ -1,10 +1,10 @@
 /*
  *
- *  jQuery $.getImageData Plugin 0.2
+ *  jQuery $.getImageData Plugin 0.3
  *  http://www.maxnov.com/getimagedata
  *  
  *  Written by Max Novakovic (http://www.maxnov.com/)
- *  Date: Mon Sep 13 2010
+ *  Date: Thu Jan 13 2011
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *  
@@ -15,13 +15,13 @@
  * 
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *  
- *  Copyright 2010, Max Novakovic
+ *  Copyright 2011, Max Novakovic
  *  Dual licensed under the MIT or GPL Version 2 licenses.
  *  http://www.maxnov.com/getimagedata/#license
  * 
  */
 
-// jquery.jsonp
+// jQuery JSONP
 (function(d,l){function w(){}function Q(a){p=[a]}function g(a,h,i){return a&&a.apply(h.context||h,i)}function x(a){function h(b){!j++&&l(function(){m();n&&(q[c]={s:[b]});y&&(b=y.apply(a,[b]));g(a.success,a,[b,z]);g(A,a,[a,z])},0)}function i(b){!j++&&l(function(){m();n&&b!=B&&(q[c]=b);g(a.error,a,[a,b]);g(A,a,[a,b])},0)}a=d.extend({},C,a);var A=a.complete,y=a.dataFilter,D=a.callbackParameter,E=a.callback,R=a.cache,n=a.pageCache,F=a.charset,c=a.url,e=a.data,G=a.timeout,o,j=0,m=w;a.abort=function(){!j++&&
 m()};if(g(a.beforeSend,a,[a])===false||j)return a;c=c||r;e=e?typeof e=="string"?e:d.param(e,a.traditional):r;c+=e?(/\?/.test(c)?"&":"?")+e:r;D&&(c+=(/\?/.test(c)?"&":"?")+encodeURIComponent(D)+"=?");!R&&!n&&(c+=(/\?/.test(c)?"&":"?")+"_"+(new Date).getTime()+"=");c=c.replace(/=\?(&|$)/,"="+E+"$1");n&&(o=q[c])?o.s?h(o.s[0]):i(o):l(function(b,k,s){if(!j){s=G>0&&l(function(){i(B)},G);m=function(){s&&clearTimeout(s);b[H]=b[t]=b[I]=b[u]=null;f[J](b);k&&f[J](k)};window[E]=Q;b=d(K)[0];b.id=L+S++;if(F)b[T]=
 F;var N=function(v){(b[t]||w)();v=p;p=undefined;v?h(v[0]):i(M)};if(O.msie){b.event=t;b.htmlFor=b.id;b[H]=function(){/loaded|complete/.test(b.readyState)&&N()}}else{b[u]=b[I]=N;O.opera?(k=d(K)[0]).text="jQuery('#"+b.id+"')[0]."+u+"()":b[P]=P}b.src=c;f.insertBefore(b,f.firstChild);k&&f.insertBefore(k,f.firstChild)}},0);return a}var P="async",T="charset",r="",M="error",L="_jqjsp",t="onclick",u="on"+M,I="onload",H="onreadystatechange",J="removeChild",K="<script/>",z="success",B="timeout",O=d.browser,
