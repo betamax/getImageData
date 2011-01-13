310468aec13da6dfbc0957a4ea747d2077fe73d5
Max Novakovic <max@maxnov.com>
2011-01-13 13:53:13 +0000

    Added node.js server

---
+++ server-examples/node/README.md
@@ -0,0 +1,79 @@
+$.getImageData: Node.js server
+==============
+
+This is an implementation of the Google App Engine server using Node.js. 
+
+Requirements
+-----
+
+ * [Node.js][node_link]
+ * [GraphicsMagick][graphicsmagick]
+
+Installation
+-----
+
+Firstly, you will need node.js. You can find [OS X install instructions here][osx_install]. If you are running a Linux distro, then [these instructions to install node.js on Ubuntu][ubuntu_install] should help you.
+
+As a dependency to the gm module, you will need to install [GraphicsMagick][graphicsmagick] which you can do with the following command (it requires [homebrew][homebrew]) on OS X:
+
+    brew install graphicsmagick
+
+To download the server, you can run this command:
+
+    git clone https://github.com/betamax/getImageData/tree/master/server-examples/node
+
+Alternatively, you can download the project from github using the download button.
+
+I *hope* that all the dependencies of the project are included with the server however, if they are not then this is the list of them:
+
+ * [express][express_link]
+ * [bufferlist][bufferlist_link]
+ * [gm][gm_link]
+ * [connect][connect_link]
+ * [http][http_link]
+
+If you have npm installed then you can install each one by running:
+
+    npm install dependency_name_here
+
+That should be all you need to do to get the server running.
+
+[osx_install]: http://shapeshed.com/journal/setting-up-nodejs-and-npm-on-mac-osx/
+[ubuntu_install]: http://www.giantflyingsaucer.com/blog/?p=894
+[graphicsmagick]: http://www.graphicsmagick.org/
+[homebrew]: https://github.com/mxcl/homebrew
+[node_link]: http://nodejs.org/
+[express_link]: http://github.com/visionmedia/express/tree/master
+[bufferlist_link]: http://github.com/substack/node-bufferlist
+[gm_link]: http://github.com/aheckmann/gm
+[connect_link]: http://registry.npmjs.org/connect/-/connect-0.5.4.tgz
+
+Running the server
+-----
+
+To run the server, in a terminal window navigate to the location of the server and then run:
+
+    node server.js
+
+This will run the server on port 3000. The URL of the server will be http://127.0.0.1:3000/ or if you are running the server on a remote server then it will be http://[server_ip]:3000/.
+
+Hooking it up to $.getImageData
+-----
+
+Using $.getImageData all you need to do to connect it to the node server is change the server parameter when initialising the plugin like so:
+
+    $.getImageData({
+		  url: "http://www.google.co.uk/images/logos/ps_logo2.png", 
+			server: "http://127.0.0.1:3000/"
+		  success: callback(image){
+			
+			}
+		});
+
+
+Changelog
+---------
+
+13/01/11
+
+ * Created the server and this documentation
---
+++ server-examples/node/server.js
@@ -0,0 +1,144 @@
+/*
+ *
+ *  jQuery $.getImageData Plugin 0.3
+ *  http://www.maxnov.com/getimagedata
+ *  
+ *  Written by Max Novakovic (http://www.maxnov.com/)
+ *  Date: Thu Jan 13 2011
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  
+ *  DESCRIPTION: Node server used to fetch an image and return 
+ *  it as a base64 encoded string. Part of jQuery getImageData 
+ *  Plugin (http://www.maxnov.com/getimagedata).
+ * 
+ *  THANKS: To bxjx (http://stackoverflow.com/users/373903) for
+ *  his support in getting this working.
+ * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  
+ *  Copyright 2011, Max Novakovic
+ *  Dual licensed under the MIT or GPL Version 2 licenses.
+ *  http://www.maxnov.com/getimagedata/#license
+ * 
+ */
+
+// Include dependencies
+require.paths.unshift(__dirname + '/lib/')
+var express = require('express'),
+request = require('request'),
+BufferList = require('bufferlist').BufferList,
+gm = require('gm'),
+http = require('http'),
+fs = require('fs'),
+sys = require('sys');
+
+// Create the server
+var app = express.createServer(
+	express.logger()
+);
+
+// When requesting the root
+app.get('/', function(req, res){
+	
+	// If a URL and callback parameters are present 
+	if(req.param("url") && req.param("callback")) {
+		
+		// Get the parameters
+		var url = unescape(req.param("url")),
+		callback = req.param("callback"),
+		
+		// Create a BufferList
+		bl = new BufferList();
+		
+		// Couldn't have done this without the help of bxjx (http://stackoverflow.com/users/373903)
+		request({ uri:url, responseBodyStream: bl}, function (error, response, body) {
+			
+			// If the request was OK
+			if (!error && response.statusCode == 200) {
+				
+				// Check if the mimetype says it is an image
+				var mimetype = response.headers["content-type"];
+				if(mimetype == "image/gif" || mimetype == "image/jpeg" || 
+					 mimetype == "image/jpg" || mimetype == "image/png" || 
+					 mimetype == "image/tiff") {
+					
+					// Create the prefix for the data URL
+					var type_prefix = "data:" + mimetype + ";base64,",
+					
+					// Get the image from the response stream as a string and convert it to base64
+					image = new Buffer(bl.toString(), 'binary'),
+					image_64 = image.toString('base64'); 
+					
+					// Concat the prefix and the image
+					image_64 = type_prefix + image_64;
+					
+					// Set width and height to 0
+					var width = 0, 
+					height = 0,
+					
+					// Get the image filename
+					filename = "/tmp/" + url.substring(url.lastIndexOf('/')+1),
+					
+					// Create a WriteStream for the image
+					out = fs.createWriteStream(filename);
+					// Save it
+					out.write(image);
+		      out.end();
+		
+					// Get the image dimensions using GraphicsMagick
+					gm(filename).size(function(err, size){
+						
+						// Delete the tmp image
+						fs.unlink(filename);
+						
+						// Error getting dimensions
+						if(err) res.send("Error getting image dimensions", 400);
+						else {
+							
+							width = size.width;
+							height = size.height;
+					
+							// The data to be returned 
+							var return_variable = {
+								"width": width,
+								"height": height,
+								"data": image_64
+							};
+					
+							// Stringifiy the return variable and wrap it in the callback for JSONP compatibility
+							return_variable = callback + "(" + JSON.stringify(return_variable) + ");";
+					
+							// Set the headers as OK and JS
+							res.writeHead(200, {'Content-Type': 'application/javascript; charset=UTF-8'});
+					
+							// Return the data
+							res.end(return_variable);
+						
+						}
+					
+					});
+					
+				// File type was not a supported image
+				} else res.send("This file type is not supported", 400);
+				
+			// Error getting the image
+			} else res.send("Third-party server error", response.statusCode);
+			
+		});
+		
+	// Missing parameters	
+	} else res.send("No URL or no callback was specified. These are required", 400);
+	
+});
+
+// Handle 404 and 500 errors
+app.get('/404', function(req, res){
+	throw new Error('Page not found');
+});
+app.get('/500', function(req, res){
+	throw new Error('Server error');
+});
+
+// Run the server on port 3000
+app.listen(3000);
