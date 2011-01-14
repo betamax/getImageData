5e6712454fff63e727b31e1e00c0a9bc1c53747c
Max Novakovic <max@maxnov.com>
2011-01-14 13:49:43 +0000

    Formatting

--- server-examples/node/server.js
+++ server-examples/node/server.js
@@ -59,9 +59,9 @@ app.get('/', function(req, res){
 				
 				// Check if the mimetype says it is an image
 				var mimetype = response.headers["content-type"];
-				if(	mimetype == "image/gif" || mimetype == "image/jpeg" || 
-					mimetype == "image/jpg" || mimetype == "image/png" || 
-					mimetype == "image/tiff" ) {
+				if(mimetype == "image/gif" || mimetype == "image/jpeg" || 
+				mimetype == "image/jpg" || mimetype == "image/png" || 
+				mimetype == "image/tiff" ) {
 					
 					// Create the prefix for the data URL
 					var type_prefix = "data:" + mimetype + ";base64,",
